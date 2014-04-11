47a18b39c6c2539ba64cd2a6464470076d27ff29
geekfreak <geekfreak@gmail.com>
2014-04-11 00:44:31 +0000

    jsbeautifier eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,49 @@
-module.exports = function(grunt) {
+"use strict";
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  , execute: {
-        target: {
-            src: ['generate_translations.js']
-        }
-    }
-  });
+(function() {
 
-  grunt.loadNpmTasks('grunt-execute');
-    
-  // Default task(s).
-  grunt.registerTask('default',['execute']);
+    module.exports = function(grunt) {
 
-};
+        // Project configuration.
+        grunt.initConfig({
+
+            pkg: grunt.file.readJSON("package.json"),
+
+            execute: {
+                target: {
+                    src: ["generate_translations.js"]
+                }
+            },
+
+            eslint: { // task
+                options: {
+                    config: "eslint.json" // custom config
+                },
+                target: ["*.js"] // array of files
+            },
+
+            jsbeautifier: {
+                files: ["*.js", "*.json", "*.css"],
+                options: {
+                    html: {},
+                    css: {},
+                    js: {
+                        braceStyle: "end-expand",
+                        indentSize: 4
+
+                    }
+                }
+            }
+
+        });
+
+        grunt.loadNpmTasks("grunt-execute");
+        grunt.loadNpmTasks("grunt-eslint");
+        grunt.loadNpmTasks("grunt-jsbeautifier");
+
+        // Default task(s).
+        grunt.registerTask("default", ["execute", "eslint", "jsbeautifier"]);
+
+    };
+
+}());
--- generate_translations.js
+++ generate_translations.js
@@ -1,97 +1,87 @@
-var https = require('https');
-var beautify = require('js-beautify').js_beautify;
-var fs = require('fs');
+"use strict";
+
+var https = require("https");
+var beautify = require("js-beautify").js_beautify;
+var fs = require("fs");
 
 var options = {
-	hostname: 'docs.google.com',
-    path: '/spreadsheet/pub?key=0AnRyC36d2KeKdGppOHNjeHpENWNMVm5UbTdMWnZkMkE&single=true&gid=0&output=txt'
-}
+    hostname: "docs.google.com",
+    path: "/spreadsheet/pub?key=0AnRyC36d2KeKdGppOHNjeHpENWNMVm5UbTdMWnZkMkE&single=true&gid=0&output=txt"
+};
 
-var request = https.request(options, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
+var request = https.request(options, function(res) {
+    var data = "";
+    res.on("data", function(chunk) {
         data += chunk;
     });
-	
-	res.on('end', function () {
-		var filename = "translations.js";
-	
-		var mydata = data.split('\n');
 
-		for (row in mydata) {
-			mydata[row] = (mydata[row].split('\t'));
-		}	 
-		
-		var headers = mydata.shift();
-		headers.shift(); // horrible side effect operation 
-		
-		var translations = {
-      "language": {
-          "comment": "lang",
-      }
-    }
-		for (header in headers) {
-				translations.language[headers[header]] = headers[header]; 
-		}
- 
-		for (row in mydata) {
-		    var rowData = {};
-		    var keyval = mydata[row].shift();
-			
-			for (header in headers) {
-					rowData[headers[header]] = mydata[row][header]; 
-			}
-		    translations[keyval] = rowData;
-		}
+    res.on("end", function() {
+        var filename = "translations.js";
 
-		var jsFile = [
-		    "/* ========================================== */;"
-		,	"/* created : " + new Date() + " */"
-		,	"/* keys : " + mydata.length + " */"
-		,	"/* ========================================== */;"
-		,	""
-		,	"var translations = " + beautify(JSON.stringify(translations)) + ";"
-		];
-		
-		fs.writeFile(filename, jsFile.join('\n'), function(err) {
-			if(err) {
-				console.log(err);
-			} else {
-				console.log(filename + " saved!");
-			}
-		}); 
+        var mydata = data.split("\n");
 
-		fs.readFile('index.template', 'utf8', function (err,data) {
-			if (err) {
-    			return console.log(err);
-  			}
+        for (var row in mydata) {
+            mydata[row] = (mydata[row].split("\t"));
+        }
 
-			headers.shift();
-			for (var target_language in headers) {
-				var copydata = data;
-				
-				var indexFile = ("index." + headers[target_language] + ".html").replace(".en","");
-                
-				console.log("generating " + indexFile);
-				for (var key in translations) {
-					var re = new RegExp("##"+key+"##", "g");
-					copydata = copydata.replace(re, translations[key][headers[target_language]]);
-				}
-				fs.writeFile(indexFile, copydata, function(err) {
-					if(err) {
-						console.log(err);
-					} else {
-						console.log(filename + " saved!");
-					}
-				}); 
-			};
-		});
+        var headers = mydata.shift();
+        headers.shift(); // horrible side effect operation 
+
+        var translations = {
+            "language": {
+                "comment": "lang"
+            }
+        };
+
+        for (var header in headers) {
+            translations.language[headers[header]] = headers[header];
+        }
+
+        for (row in mydata) {
+            var rowData = {};
+            var keyval = mydata[row].shift();
+
+            for (header in headers) {
+                rowData[headers[header]] = mydata[row][header];
+            }
+            translations[keyval] = rowData;
+        }
+
+        var jsFile = [
+            "/* ========================================== */", "/* created : " + new Date() + " */", "/* keys : " + mydata.length + " */", "/* ========================================== */", "", "var translations = " + beautify(JSON.stringify(translations)) + ";"
+        ];
+
+        fs.writeFile(filename, jsFile.join("\n"), function(err) {
+            console.log(err ? err : filename + " saved!");
+        });
+
+        fs.readFile("index.template", "utf8", function(err, data) {
+            if (err) {
+                return console.log(err);
+            }
+
+            headers.shift();
+            for (var targetLanguage in headers) {
+                var copydata = data;
+
+                var indexFile = ("index." + headers[targetLanguage] + ".html").replace(".en", "");
+
+                for (var key in translations) {
+                    var re = new RegExp("##" + key + "##", "g");
+                    copydata = copydata.replace(re, translations[key][headers[targetLanguage]]);
+                }
+
+                fs.writeFile(indexFile, copydata, function(err) {
+                    console.log(err ? err : indexFile + " saved!");
+                });
+            }
+
+        });
     });
 });
 
-request.on('error', function (e) {
+request.on("error", function(e) {
     console.log(e.message);
 });
-
 
 request.end();

cdf22980da2738b7fe5ba93642317bf8ad5e699a
ʞɐǝɹɟʞǝǝƃ <davey@geekfreak.com>
2014-07-27 01:00:32 +0000

    process-v2

--- index.js
+++ index.js
@@ -34,4 +34,38 @@ $( ".btn-next" ).click( function() {
 
 $( window ).resize( function() {
   equalHeights( "#carousel1 blockquote" );
+  $( "#process-diagram" ).attr( "max-width", $( ".circle-icon" ).width() * 2 );
+  console.log( "resize" );
+} );
+
+$( "#carousel2" ).carousel( {
+  interval: 5000
+} );
+
+var clickEvent = false,
+  rotate = [ "rotate-deg270", "rotate-deg0", "rotate-deg90", "rotate-deg180" ],
+  processCarousel = $( "#process-carousel" );
+
+$( "#carousel2" ).on( "click", ".nav a", function() {
+  clickEvent = true;
+  $( ".nav li", processCarousel ).removeClass( "active" );
+  $( this ).parent().addClass( "active" );
+} ).on( "slid.bs.carousel", function() {
+  if ( !clickEvent ) {
+
+    var count = $( ".nav", processCarousel ).children().length - 1,
+      current = $( ".nav li.active", processCarousel ),
+      id = parseInt( current.data( "slide-to" ) );
+
+    current.removeClass( "active" ).next().addClass( "active" );
+    console.log( count, id, rotate[ id ] );
+    if ( count === id ) {
+      $( ".nav li", processCarousel ).first().addClass( "active" );
+    }
+
+    $( ".process-diagram", processCarousel ).removeClass( function( index, css ) {
+      return ( css.match( /(^|\s)rotate-\S+/g ) || [] ).join( " " );
+    } ).addClass( rotate[ id ] );
+  }
+  clickEvent = false;
 } );

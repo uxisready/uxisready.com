fac0dec5fab6b30f1bb33ec6beaf2d4ff3126046
ʞɐǝɹɟʞǝǝƃ <davey@geekfreak.com>
2014-07-29 17:47:46 +0000

    new images

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function equalHeights( selector ) {
 
 }
 
-equalHeights( "#carousel1 blockquote" );
+equalHeights( "#carousel1 blockquote, .chevron img" );
 $( "#carousel1" ).carousel( {
   interval: 5500
 } );
@@ -34,52 +34,50 @@ $( ".btn-next" ).click( function() {
 
 $( window ).resize( function() {
   equalHeights( "#carousel1 blockquote" );
-//  $("#process-diagram").attr("max-width",$(".circle-icon").width()*3);
+  //  $("#process-diagram").attr("max-width",$(".circle-icon").width()*3);
 } );
 
-$("#process-carousel").carousel({
+$( "#process-carousel" ).carousel( {
   interval: 0
-});
+} );
 
-var clickEvent = false
-  , processCarousel = $("#process-carousel")
-  , processNav = $(".process-nav")
-  ;
+var clickEvent = false,
+  processCarousel = $( "#process-carousel" ),
+  processNav = $( ".process-nav" );
 
-function setProcessTab(tab) {
-   $(".nav li").removeClass("active");
-  tab.addClass("active");
+function setProcessTab( tab ) {
+  $( ".nav li" ).removeClass( "active" );
+  tab.addClass( "active" );
 
 }
 
-$(".our-process").on("click", ".nav a", function () {
+$( ".our-process" ).on( "click", ".nav a", function() {
   clickEvent = true;
-  console.log("nav click");
-  $(".nav li").removeClass("active");
-  $(this).parent().addClass("active");
+  console.log( "nav click" );
+  $( ".nav li" ).removeClass( "active" );
+  $( this ).parent().addClass( "active" );
   //setProcessTab($(this).parent());
-}).on("slid.bs.carousel", function () {
-  if (!clickEvent) {
+} ).on( "slid.bs.carousel", function() {
+  if ( !clickEvent ) {
 
-    var current = $("li.active", processNav)
-      , id = parseInt(current.data("slide-to"))
-      ;
+    var current = $( "li.active", processNav ),
+      id = parseInt( current.data( "slide-to" ) );
 
-    current.removeClass("active").next().addClass("active");
+    current.removeClass( "active" ).next().addClass( "active" );
 
-    if (id === 0) {
-      $("li", processNav).first().addClass("active");
+    if ( id === 0 ) {
+      $( "li", processNav ).first().addClass( "active" );
     }
-    
-//    var rotate = [ "rotate-deg270" , "rotate-deg0" , "rotate-deg90" , "rotate-deg0" ];
-//    $(".process-diagram", processCarousel).removeClass (function (index, css) {
-//      return (css.match (/(^|\s)rotate-\S+/g) || []).join(" ");
-//    }).addClass(rotate[id]);
-    
-    $(".dots li").removeClass("active");
-    $(".dots li:eq("+id+")").addClass("active");
+
+    //    var rotate = [ "rotate-deg270" , "rotate-deg0" , "rotate-deg90" , "rotate-deg0" ];
+    //    $(".process-diagram", processCarousel).removeClass (function (index, css) {
+    //      return (css.match (/(^|\s)rotate-\S+/g) || []).join(" ");
+    //    }).addClass(rotate[id]);
+
+    $( ".dots li" ).removeClass( "active" );
+    $( ".dots li:eq(" + id + ")" ).addClass( "active" );
 
   }
-    
+
   clickEvent = false;
-});
+} );

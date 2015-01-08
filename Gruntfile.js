7ce8116d80204e2b5f3878a6a6cfa853fe53bde7
ʞɐǝɹɟʞǝǝƃ <davey@geekfreak.com>
2015-01-08 16:22:37 +0100

    merging refactored code

---
+++ Gruntfile.js
@@ -0,0 +1,167 @@
+( function() {
+
+  "use strict";
+
+  module.exports = function( grunt ) {
+
+    // Project configuration.
+    grunt.initConfig( {
+
+      pkg: grunt.file.readJSON( "package.json" ),
+
+      execute: {
+        target: {
+          src: [ "build/build.js" ]
+        }
+      },
+      banner: "/* Author: uxisready.com , <%= grunt.template.today(\"yyyy\") %> */\n",
+      stylus: {
+        compile: {
+          options: {
+            compress: true,
+           // banner: "<%= banner =%>",
+            // paths: ["path/to/import", "another/to/import"],
+            urlfunc: "embedurl", // use embedurl("test.png") in our code to trigger Data URI embedding
+            use: [],
+            import: [ //  @import "foo", "bar/moo", etc. into every .styl file
+              // "stylus/", //  that is compiled. These might be findable based on values you gave
+              //            "_mixins"    //  to `paths`, or a plugin you added under `use`
+            ]
+          },
+          files: {
+
+            "src/tmp/css/style.css"          : "src/stylus/style.styl",
+            "src/tmp/css/style-noscript.css" : "src/stylus/style-noscript.styl",
+
+            "src/tmp/css/style-wide.css"     : "src/stylus/style-wide.styl",
+            "src/tmp/css/style-normal.css"   : "src/stylus/style-normal.styl",
+            "src/tmp/css/style-narrow.css"   : "src/stylus/style-narrow.styl",
+            "src/tmp/css/style-narrower.css" : "src/stylus/style-narrower.styl",
+            "src/tmp/css/style-mobile.css"   : "src/stylus/style-mobile.styl",
+
+            "src/tmp/css/style-ie.css"       : "src/stylus/style-ie.styl",
+            "src/tmp/css/ie/v8.css"          : "src/stylus/ie/v8.styl",
+            "src/tmp/css/ie/v9.css"          : "src/stylus/ie/v9.styl",
+
+            "src/tmp/css/print.css"          : "src/stylus/print.styl",
+            "src/tmp/css/screen.css"         : "src/stylus/screen.styl"
+
+            //"path/to/another.css": ["path/to/sources/*.styl", "path/to/more/*.styl"] // compile and concat into single file
+          }
+        }
+      },
+
+      jsbeautifier: {
+        files: [ "src/*.js", "*.json", "src/css/*.css" ],
+        options: {
+          js: {
+            braceStyle: "end-expand",
+            indentSize: 2,
+            preserveNewlines: true,
+            maxPreserveNewlines: 10,
+            jslintHappy: false,
+            spaceInParen: true,
+            goodStuff: true
+          }
+        }
+      },
+
+      eslint: { // task
+        options: {
+          config: "eslint.json" // custom config
+        },
+        target: [ "*.js" ] // array of files
+      },
+
+      jscs: { // task
+        src: "src/tmp/js/*.js",
+        options: {
+          config: "build/jscs.json",
+          validateQuoteMarks: "\x22"
+        }
+      },
+
+      copy: {
+        main: {
+          files: [
+            { expand: true,  cwd: "src/tmp/",  src: ["*.html","css/**"],  dest: "www/" },
+            { expand: true,  cwd: "src/",      src: ["fonts/**", "images/**", "js/**", "css/**" ],  dest: "www/" }
+          ]
+        }
+      },
+
+      concat: {
+        options: {
+          // define a string to put between each file in the concatenated output
+          separator: "\n;" + Array(80).join("/") + "\n\n"
+        },
+        deploy: {
+          files: {
+            "src/tmp/concat/<%= pkg.name %>.js": ["src/js/jquery.min.js","src/js/libs/*.js", "src/js/init.js", "src/js/skel*.js", "src/js/index.js"]
+           },
+          nonull:true
+        }
+      },
+
+      cssmin: {
+        css:{
+          src: "src/tmp/**.css",
+          dest: "src/tmp/<%= pkg.name %>.min.css"
+        }
+      },
+
+      uglify: {
+        options: {
+          banner: "/*! <%= pkg.name %> (<%= pkg.version %>) <%= grunt.template.today(\"dd-mm-yyyy\") %> */\n"
+        },
+        dist: {
+          files: {
+            "src/tmp/min/<%= pkg.name %>.min.js": ["src/tmp/concat/*.js"]
+          }
+        }
+      },
+
+      clean: {
+        cleantmp: ["src/tmp/"],
+        cleanwww: ["www/"]
+      },
+
+      watch: {
+        files: [ "src/**/*" ],
+        tasks: [ "quick" ]
+      }
+
+    } );
+
+    grunt.loadNpmTasks( "grunt-execute" );
+    grunt.loadNpmTasks( "grunt-eslint" );
+    grunt.loadNpmTasks( "grunt-jscs-checker" );
+    grunt.loadNpmTasks( "grunt-mkdir" );
+    grunt.loadNpmTasks( "grunt-jsbeautifier" );
+    grunt.loadNpmTasks( "grunt-contrib-stylus" );
+    grunt.loadNpmTasks( "grunt-contrib-copy" );
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+    grunt.loadNpmTasks( "grunt-contrib-concat" );
+    grunt.loadNpmTasks( "grunt-contrib-uglify" );
+    grunt.loadNpmTasks( "grunt-contrib-cssmin" );
+    grunt.loadNpmTasks( "grunt-contrib-clean" );
+
+    // Default task(s).
+    grunt.registerTask( "full"     , [ "clean", "stylus", "execute", "jsbeautifier", "eslint", "jscs" , "minimize" , "copy"] );
+
+    grunt.registerTask( "default"  , [ "tidy", "generate", "lint" , "minimize" , "copy"] );
+
+    grunt.registerTask( "tidy"     , [ "clean" ] );
+    grunt.registerTask( "generate" , [ "stylus", "execute" ] );
+
+    grunt.registerTask( "lint"     , [ "jsbeautifier", "eslint", "jscs" ] );
+
+    grunt.registerTask( "debug"    , [ "generate", "lint" ] );
+    grunt.registerTask( "quick"    , [ "stylus", "jsbeautifier" ] );
+    grunt.registerTask( "minimize" , [ "cssmin", "concat" ] );
+
+    grunt.registerTask( "deploy"   , [ "" ] );
+
+  };
+
+}() );
---
+++ src/js/index.js
@@ -0,0 +1,123 @@
+"use strict";
+
+$.fn.fitText = function( kompressor, options ) {
+
+  var compressor = kompressor || 1,
+    settings = $.extend( {
+      "minFontSize": parseFloat( Number.NEGATIVE_INFINITY ),
+      "maxFontSize": parseFloat( Number.POSITIVE_INFINITY )
+    }, options );
+
+  return this.each( function() {
+
+    var $this = $( this );
+
+    // Resizer() resizes items based on the object width divided by the compressor * 10
+    var resizer = function() {
+      $this.css( "font-size", Math.max( Math.min( $this.width() / ( compressor * 10 ), settings.maxFontSize ), settings.minFontSize ) );
+    };
+
+    $( window )
+      .on( "resize.fittext orientationchange.fittext", resizer )
+      .trigger( "resize.fittext" );
+
+  } );
+
+};
+
+$.fn.random = function() {
+  return $( this[ Math.floor( Math.random() * this.length ) ] );
+};
+
+$.fn.equalHeights = function( pad ) {
+
+  var maxHeight = 0;
+
+  $( this )
+    .height( "auto" )
+    .each( function() {
+      if ( $( this ).outerHeight() > maxHeight ) {
+        maxHeight = $( this ).outerHeight();
+      }
+    } )
+    .height( 0 + maxHeight + ( pad || 20 ) );
+
+  return this;
+
+};
+
+function stepCarousel() {
+  $( ".process-nav li" ).removeClass( "active" ).random().addClass( "active" );
+  var content = ( ".process-nav li" );
+  return content;
+}
+
+function startCarousel() {
+  return setInterval( stepCarousel, 500 );
+}
+
+function stopCarousel( nIntervId ) {
+  clearInterval( nIntervId );
+}
+
+var initialize = function() {
+
+  $( "HTML" ).show();
+
+  $( window ).on( "resize", function() {
+    $( ".carousel-inner > DIV" ).equalHeights();
+    $( ".quotes" ).equalHeights();
+  } ).resize();
+
+  $( ".fit-text" ).fitText( 0.8, {
+    minFontSize: "20px",
+    maxFontSize: "40px"
+  } );
+
+  $( ".fit-text-66" ).fitText( 2, {
+    maxFontSize: "60px"
+  } );
+
+  $( ".quotes" ).removeClass( "active" ).random().addClass( "active" );
+
+  $( ".process-nav li" ).click( function( e ) {
+
+    /* Act on the event */
+    e.preventDefault();
+
+    var selectedTab = $( this );
+    var content = $( "#carousel2 .item" );
+    var target = parseInt( selectedTab.attr( "data-slide-to" ) );
+
+    selectedTab.siblings().removeClass( "active" );
+    selectedTab.addClass( "active" );
+
+    content.removeClass( "active" ).eq( target ).addClass( "active" );
+
+  } );
+
+  //
+  //TODO add timer function
+  //TODO enable quote carousel
+  //
+
+};
+
+$( "HTML" ).hide();
+$( initialize );
+
+<!-- Analytics ============================================================= -->
+( function( i, s, o, g, r, a, m ) {
+  i[ "GoogleAnalyticsObject" ] = r;
+  i[ r ] = i[ r ] || function() {
+    ( i[ r ].q = i[ r ].q || [] ).push( arguments );
+  }, i[ r ].l = 1 * new Date();
+  a = s.createElement( o ),
+  m = s.getElementsByTagName( o )[ 0 ];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore( a, m );
+} )( window, document, "script", "https://www.google-analytics.com/analytics.js", "ga" );
+ga( "create", "UA-49766755-1", "uxisready.com" );
+ga( "send", "pageview" );
+<!-- ======================================================================= -->
